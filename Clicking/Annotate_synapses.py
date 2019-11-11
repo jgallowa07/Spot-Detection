314addcc3a073f3de4b9438d5366b398bc8d68b4
jgallowa07 <jaredgalloway07@gmail.com>
2019-11-11 14:58:41 -0800

    cleanup up clicking script

--- Clicking/Annotate_synapses.py
+++ Clicking/Annotate_synapses.py
@@ -4,56 +4,38 @@ File: Helpers.py
 Authors Jared Galloway, Nick Wagner, Annie Wang
 Date: 11/20/2019
 
-This file contains all helpful python
-functions for scripts included in synapse detection.
+This file contains the script to annotate a synapse image.
+This will annotate one image specified by the path 
+given to --image, with output witten to --out, and 
+--bbox representing the size of the bounding box around 
+clicking position. 
 """
-
-
-# TODO : Finish commenting and finish thes function.
-# TODO : UNDER CONSTRUCTION
-# next we can impliment a 
-
-
-##############################################################################
-
-#import os
-#import sys
-
-#import numpy as np
-#import matplotlib.pyplot as plt
-#from PIL import Image
-#from skimage.draw import circle
-#import cv2
-
-
-# TODO Argparse
-
 
 import argparse
 import cv2
- 
+
+##############################################################################
  
 def annotate(event, x, y, flags, param):
- 
-	# if the left mouse button was clicked, record the starting
-	# (x, y) coordinates and indicate that cropping is being
-	# performed
+
+    # to make clicking more accurate, for me the puts
+    # the x,y coord EXACTLY where the point of my clicker is.
+    x -= 1 if x >= 1 else x
+    y -= 2 if x >= 2 else x
+
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(x,y)
         
-        # place a circle in
+        # place a circle in where we're clicking
         cv2.circle(image, (x,y), 1, (255,0,255), -1)
         top_coord = (int(x-box_adj),int(y-box_adj))
         bot_coord = (int(x+box_adj),int(y+box_adj))
+
+        # Bounding box
         cv2.rectangle(image,top_coord,bot_coord,(255,0,0),0)
-        
-        print(param[0])
         meta = [param[0],top_coord[0],top_coord[1],bot_coord[0],bot_coord[1]]
-        param[1].write(f"{','.join([str(bb) for bb in meta])}")
+        param[1].write(f"{','.join([str(bb) for bb in meta])}\n")
  
-		# draw a rectangle around the region of interest
-#		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
-#		cv2.imshow("image", image)
+##############################################################################
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -67,13 +49,11 @@ ap.add_argument("-bbox", "--bounding_box_size", type=int,
     default is 4")
 args = vars(ap.parse_args())
 
-#if(args[output] == None):
-#    base=os.path.basename(filepath)
-#    output = os.path.splitext(base)[0] + ".csv"
+##############################################################################
+
 output_fp = open(args["out"],"a")
 box_adj = args["bounding_box_size"]
 
- 
 # load the image, clone it, and setup the mouse callback function
 image = cv2.imread(args["image"])
 clone = image.copy()
@@ -88,67 +68,7 @@ while True:
     if cv2.waitKey(20) & 0xFF == 27:
         break
 
+output_fp.close()
 cv2.destroyAllWindows()
-
-
-"""
-
-img = None
-
-
-##############################################################################
-# UNDER CONSTRUCTION
-
-
-def annotate(event, x, y, flags, params):
-    print("in annotate")
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(f":x {x}, y: {y}")
-         
-        cv2.circle(img,(x,y),100,(255,0,255),-1)
-        #top_coord = (int(x-box_adj),int(y-box_adj))
-        #bot_coord = (int(x+box_adj),int(y+box_adj))
-        #cv2.rectangle(img,top_coord,bot_coord,(255,0,0),0)
-        #print(params[0])
-        
-        #params[0].write(f{})
-        
-        
-
-##############################################################################
-
-def annotate_by_clicking(filepath, boxsize=16, output=None):
-
-    # By default, we will write a csv with the 
-    # same filemae as image, but with csv exgtention and in the cwd. 
-    if(output == None):
-        base=os.path.basename(filepath)
-        output = os.path.splitext(base)[0] + ".csv"
-    #output_fp = open()
-    
-    #print(output)
-    #sys.exit()
-    
-    
-
-    img = cv2.imread(filepath)
-    cv2.namedWindow('img')    
-    cv2.setMouseCallback('img', annotate, [filepath,output])
-
-    while True:
-        cv2.imshow('img',img)
-        if cv2.waitKey(20) & 0xFF == 27:
-            break
-    
-    cv2.destroyAllWindows()
-    #.close()
-
-"""
-
-
-
-
-
-
 
 
