21038478b63641ea320468f98088bb0c03827545
Nwagner22 <nwagner2@uoregon.edu>
2019-11-17 14:36:22 -0800

    initial model work

---
+++ DeepLearning/networks.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+
+def initial_CNN_map(data):
+    """
+    This function is in charge of taking in the testing and training data and
+    returning a model object that we can use within train.py. This model will
+    be creating a final map output where 1s are synapses and 0s are not.
+
+    params:
+
+    This function returns a TensorFlow model object
+    """
+
+    
+
+
+
+def initial_CNN_count(data):
+    """
+    This function is in charge of taking in the testing and training data and
+    returning a model object that we can use within train.py. This model will
+    be creating a final output that is just a count of synapses detected.
+
+    params:
+
+    This function returns a TensorFlow model object
+    """
