adf40fd5ef181624102e6d07d5df375d181fb763
jgallowa07 <jaredgalloway07@gmail.com>
2019-12-23 16:24:18 -0700

    changed out put of pixel anno to pixelmap np dump

--- Clicking/Pixel_Annotate_synapses.py
+++ Clicking/Pixel_Annotate_synapses.py
@@ -15,18 +15,23 @@ ap.add_argument("-i", "--image", required=True, help="Path to the image")
 ap.add_argument("-o", "--out", required=True, help="Path to the annotation csv")
 args = vars(ap.parse_args())
 
+# a unique set of clicked points
 clicked_points = set()
+
+# load the numpy image, clone it, and setup the mouse callback function
+img = np.load(args["image"], allow_pickle = True)
+
+# initialize an empty pixelmap for output
+pixelmap = np.zeros(img.shape[:-1]).astype(np.int)
  
-def annotate(event, x, y, flags, param):
+def annotate(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN and (x,y) not in clicked_points:
         clicked_points.add((x,y))
         cv2.circle(image, (x,y), 0, (255,0,255), -1)
-        param[1].write(f"{x},{y}\n")
+        pixelmap[x,y] = 1
 
-output_fp = open(args["out"],"a")
+#output_fp = open(args["out"],"a")
 
-# load the image, clone it, and setup the mouse callback function
-img = np.load(args["image"], allow_pickle = True)
 cv2.imwrite('color_img.jpg', img)
 cv2.namedWindow('image',cv2.WINDOW_NORMAL)
 cv2.resizeWindow('image', 600,600)
@@ -34,17 +39,18 @@ cv2.imshow("image", img);
 image = cv2.imread("color_img.jpg")
 clone = image.copy()
 cv2.namedWindow("image")
-cv2.setMouseCallback("image", annotate, [args["image"],output_fp])
+cv2.setMouseCallback("image", annotate, [args["image"],pixelmap])
  
-# keep looping until the 'q' key is pressed
+# keep looping until the <esc> key is pressed
+# display the image and wait for a keypress
 while True:
-	# display the image and wait for a keypress
     cv2.imshow("image", image)
     key = cv2.waitKey(1) & 0xFF
     if cv2.waitKey(20) & 0xFF == 27:
         break
 
-output_fp.close()
 cv2.destroyAllWindows()
+np.transpose(pixelmap).dump(args["out"])
+
 
 
